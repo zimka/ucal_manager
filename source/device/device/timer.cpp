d313dc30f8973141a012c83d8d93a3b0106c733b
Boris Zimka <zimka.b@ya.ru>
2019-05-09 16:52:12 +0300

    Add DeviceTimer implementation

--- source/device/device/timer.cpp
+++ source/device/device/timer.cpp
@@ -1,22 +1,67 @@
 #include "timer.h"
+#include "common/exceptions.h"
 using namespace device;
 
-DeviceTimer::DeviceTimer(common::TimeUnit step){}
-common::TimeStamp DeviceTimer::getStamp(){
-	return common::TimeStamp(0, 0);
+DeviceTimer::DeviceTimer(common::TimeUnit step){
+	step_ = step;
+	overdue_ = 0;
+	start_ = clock_.now();
 };
+
+common::TimeStamp DeviceTimer::getStamp() const{
+	if (! isRunning()){
+		common::AssertionError(
+			"Non-running DeviceTimer is requested for TimeStamp"
+		);
+	}
+	common::TimeUnit duration = (std::chrono::duration_cast<std::chrono::milliseconds>(clock_.now() - start_)).count();
+	return common::TimeStamp(step_, duration/step_);
+};
+
 bool DeviceTimer::setStep(common::TimeUnit step){
-	return false;
+	if (isRunning()){
+		return false;
+	}
+	step_ = step;
+	return true;
 };
+
 bool DeviceTimer::run(){
-	return false;
+	if (is_running_){
+		return false;
+	}
+	start_ = clock_.now();
+	is_running_ = true;
+	return true;
 };
+
 bool DeviceTimer::stop(){
-	return false;
+	if (! is_running_){
+		return false;
+	}
+	is_running_ = false;
+	return true;
 };
-bool DeviceTimer::setOverdue(common::TimeUnit t){
-	return false;
+
+bool DeviceTimer::setOverdue(common::TimeUnit overdue){
+	if (isRunning()){
+		return false;
+	}
+	overdue_ = overdue;
+	return true;
 };
+
 bool DeviceTimer::isOverdue() const{
-	return false;
+	if (! isRunning()){
+		return false;
+	}
+	if (!overdue_){
+		return false;
+	}
+	common::TimeUnit duration = (std::chrono::duration_cast<std::chrono::milliseconds>(clock_.now() - start_)).count();
+	return duration > overdue_;
+};
+
+bool DeviceTimer::isRunning() const{
+	return is_running_;
 };
--- source/device/device/timer.h
+++ source/device/device/timer.h
@@ -8,12 +8,19 @@ namespace device{
 	class DeviceTimer{	
 	public:
 		DeviceTimer(common::TimeUnit step);
-		common::TimeStamp getStamp();
+		common::TimeStamp getStamp() const;
 		bool setStep(common::TimeUnit step);
 		bool run();
 		bool stop();
-		bool setOverdue(common::TimeUnit t);
+		bool setOverdue(common::TimeUnit overdue);
 		bool isOverdue() const;
+		bool isRunning() const;
+	private:
+		bool is_running_ = false;
+		common::TimeUnit step_;
+		common::TimeUnit overdue_;
+		std::chrono::steady_clock clock_;
+		std::chrono::time_point<std::chrono::steady_clock> start_;
 	};
 }
 
